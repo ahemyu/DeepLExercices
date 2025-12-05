0dba35a607cdbc38f4ba9df2c0dcda266f24d920
ahemyu <hansovic7@gmail.com>
2025-12-05 09:34:39 +0100

    ex2: implement SgdWithMomentum and Adam

--- exercise2_material/src_to_implement/Optimization/Optimizers.py
+++ exercise2_material/src_to_implement/Optimization/Optimizers.py
@@ -6,10 +6,60 @@ class Sgd:
     def calculate_update(self, weight_tensor, gradient_tensor):
         """returns the updated weights according to the basic gradient descent update scheme"""
         
-        
-        # TODO: calculate the loss with respect to the weights
         # we already have the gradient_tensor so we just need to subtract the gradients scaled with lr from the weights
         weight_tensor = np.array(weight_tensor)
         gradient_tensor = np.array(gradient_tensor)
         
         return weight_tensor - (self.learning_rate * gradient_tensor) 
+
+
+class SgdWithMomentum: 
+    def __init__(self, learning_rate: float, momentum_rate: float):
+        self.learning_rate = learning_rate
+        self.momentum_rate = momentum_rate
+        self.momentum = None
+    
+    def calculate_update(self, weight_tensor, gradient_tensor): 
+
+        weight_tensor = np.array(weight_tensor)
+        gradient_tensor = np.array(gradient_tensor)
+        # if momentum is not inited yet, do it
+        if not self.momentum: 
+            self.momentum = np.zeros_like(weight_tensor)
+
+        # update momentum
+        self.momentum = self.momentum_rate * self.momentum - self.learning_rate * gradient_tensor
+        # update weights and return them 
+        return self.momentum + weight_tensor 
+        
+
+class Adam: 
+    def __init__(self,learning_rate, mu, rho):
+        
+        self.learning_rate = learning_rate
+        self.mu = mu
+        self.rho = rho
+        self.first_momentum = None
+        self.second_momentum = None
+        self.iteration = 0
+
+    def calculate_update(self, weight_tensor, gradient_tensor): 
+
+        weight_tensor = np.array(weight_tensor)
+        gradient_tensor = np.array(gradient_tensor)
+        self.iteration += 1        
+
+        # we only need to check one of them as they are always used together
+        if not self.first_momentum or not self.second_momentum: 
+            self.first_momentum = np.zeros_like(weight_tensor)
+            self.second_momentum = np.zeros_like(weight_tensor)
+        
+        self.first_momentum = self.mu * self.first_momentum + (1-self.mu) * gradient_tensor # mu controls how much we remember and how much emphasis we give to current gradient
+        self.second_momentum = self.rho * self.second_momentum + (1-self.rho) * (gradient_tensor * gradient_tensor) # stores the magnitude of the gradient
+        # we correct the 0 shifted bias
+        corrected_first = self.first_momentum / (1 - pow(self.mu, self.iteration)) 
+        corrected_second = self.second_momentum / (1 - pow(self.rho, self.iteration))
+        
+        # ratio of velocity and magnitude of gradient change
+        return weight_tensor - self.learning_rate * corrected_first / (np.sqrt(corrected_second) + 1e-8)
+
