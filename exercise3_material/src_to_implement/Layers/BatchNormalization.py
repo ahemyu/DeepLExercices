4e63f0432ec0ee378a89762c4a436ce6922fe315
ahemyu <hansovic7@gmail.com>
2026-01-06 09:58:45 +0100

    ex3: BatchNorm: init weights and bias

--- exercise3_material/src_to_implement/Layers/BatchNormalization.py
+++ exercise3_material/src_to_implement/Layers/BatchNormalization.py
@@ -1,3 +1,19 @@
-class BatchNormalization:
-    def __init__(self):
-        pass
+from Layers import Base
+import numpy as np
+
+class BatchNormalization(Base.BaseLayer):
+
+    def __init__(self, channels):
+        super().__init__(trainable=True)
+        self.channels = channels
+        self.initialize()
+
+    def initialize(self): 
+        self.weights = np.zeros(self.channels)
+        self.bias = np.ones(self.channels)
+
+    def forward(self, input_tensor): 
+        return input_tensor
+
+    def backward(self, error_tensor): 
+      return error_tensor
