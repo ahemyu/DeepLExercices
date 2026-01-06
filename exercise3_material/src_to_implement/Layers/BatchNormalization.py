a69d0fd65b9ea5289fa7f045401893975354c1d6
ahemyu <hansovic7@gmail.com>
2026-01-06 11:07:37 +0100

    ex3: BatchNormLayer: forward for training with 2d Input works

--- exercise3_material/src_to_implement/Layers/BatchNormalization.py
+++ exercise3_material/src_to_implement/Layers/BatchNormalization.py
@@ -9,11 +9,24 @@ class BatchNormalization(Base.BaseLayer):
         self.initialize()
 
     def initialize(self): 
-        self.weights = np.zeros(self.channels)
-        self.bias = np.ones(self.channels)
+        self.weights = np.ones(self.channels)
+        self.bias = np.zeros(self.channels)
 
     def forward(self, input_tensor): 
-        return input_tensor
+        # an example shape of input could be  (200, 2, 3, 3) (so batches, channels, rows, cols)
+        #First do the Fully Connected Layer approach
+        #calculate X tilde per batch
+        # for that, calculate batch mean and std
+        mean_batch = np.mean(input_tensor, axis=0)
+        std_batch = np.std(input_tensor, axis=0)
+        x_tilde = (input_tensor - mean_batch) / np.sqrt(std_batch**2 + 1e-11)
+
+        #  calculate the output for this layer as x_tilde * weights (elementwise) + bias
+        output = self.weights * x_tilde + self.bias
+        
+        return output
 
     def backward(self, error_tensor): 
+        #TODO: calculate gradient w.r.t weights (to update the weights)
+        #TODO: calculate gradient w.r.t. input (for propogating loss, use helper function compute_bn_gradients)
       return error_tensor
